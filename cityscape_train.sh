e20f018bbba12733fc70b3d9a91345e3c750f1cf
rotems <rotems@weizmann.ac.il>
2020-02-22 20:11:14 +0200

    fix

--- cityscape_train.sh
+++ cityscape_train.sh
@@ -1,1 +1,1 @@
-/opt/conda/bin/python train.py --backbone resnet --lr 0.01 --workers 4 --epochs 40 --batch-size 16 --gpu-ids 0 --checkname deeplab-resnet_cc --eval-interval 1 --dataset cityscapes
+nohup /opt/conda/bin/python /home/labs/waic/rotems/code/pytorch-deeplab-xception/train.py --backbone resnet --lr 0.01 --workers 4 --epochs 40 --batch-size 16 --gpu-ids 0 --checkname deeplab-resnet_cc_ce_full --eval-interval 1 --dataset cityscapes &
